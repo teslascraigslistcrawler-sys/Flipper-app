9e4c51c49865fd819b6fcba912d7529517cb42c1
apophas <teslascraigslistcrawler@gmail.com>
2026-04-20 01:56:14 +0000

    Checkpoint before smart query + condition pricing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/.env" });
 'use strict';
-
-require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
